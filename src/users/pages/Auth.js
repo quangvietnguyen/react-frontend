f2a710c4aa2f7718746133383b090056b26eb87d
nguye374 <nguye374@sheridancollege.ca>
2021-10-27 01:08:01 -0400

    Oct 27 2nd

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -9,8 +9,10 @@ import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import "./Auth.css";
 import Card from "../../shared/components/UIElements/Card";
+import { AuthContext } from "../../shared/context/auth-context";
 
 const Auth = () => {
+  const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState();
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -29,6 +31,7 @@ const Auth = () => {
   const authSubmitHandler = (event) => {
     event.preventDefault();
     console.log(formState.inputs);
+    auth.login();
   };
 
   const switchModeHandler = () => {
